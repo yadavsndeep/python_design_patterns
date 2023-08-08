7344597ca849fa238674e3bb115318a4b52052cd
sanyadav <sanyadav@expediagroup.com>
2023-08-08 19:57:45 +0100

    factory

---
+++ creational/factory/language_factory.py
@@ -0,0 +1,46 @@
+'''
+This script demonstartes creation of objects using factory pattern
+'''
+class English():
+    def __init__(self):
+        pass
+
+    def greet(self,name):
+        return 'Hello ' + name + ' ! How are you Doing?'
+
+
+class German():
+    def __init__(self):
+        pass
+
+    def greet(self,name):
+        return 'Hallo ' + name + ' ! Wie Gehts?'
+
+class LanguageFactory():
+
+    def create_language(self,language):
+        if language == 'German':
+            return German()
+        else:
+            return English()
+
+
+
+
+def main():
+    language_factory = LanguageFactory()
+    lang_code = input("Enter your langugae : ")
+    name = input("Enter your Name : ")
+
+    greeter = language_factory.create_language(lang_code)
+    print(greeter.greet(name))
+
+
+
+
+
+if __name__ == "__main__" :
+    main()
+
+
+    
